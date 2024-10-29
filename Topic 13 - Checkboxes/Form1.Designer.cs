2eac07191510a4f3d3c2f3b7ae16603aa3bc392e
Sam Ahrens <339729139@GoTVDSB.ca>
2024-10-29 09:29:16 -0400

    Add project files.

---
+++ Topic 13 - Checkboxes/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Topic_13___Checkboxes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkStarTrek = new System.Windows.Forms.CheckBox();
+            this.chkStarWars = new System.Windows.Forms.CheckBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblInstruction = new System.Windows.Forms.Label();
+            this.lblResults = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // chkStarTrek
+            // 
+            this.chkStarTrek.AutoSize = true;
+            this.chkStarTrek.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkStarTrek.Location = new System.Drawing.Point(74, 121);
+            this.chkStarTrek.Name = "chkStarTrek";
+            this.chkStarTrek.Size = new System.Drawing.Size(241, 41);
+            this.chkStarTrek.TabIndex = 0;
+            this.chkStarTrek.Text = "I like Star Trek";
+            this.chkStarTrek.UseVisualStyleBackColor = true;
+            // 
+            // chkStarWars
+            // 
+            this.chkStarWars.AutoSize = true;
+            this.chkStarWars.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkStarWars.Location = new System.Drawing.Point(475, 121);
+            this.chkStarWars.Name = "chkStarWars";
+            this.chkStarWars.Size = new System.Drawing.Size(253, 41);
+            this.chkStarWars.TabIndex = 1;
+            this.chkStarWars.Text = "I like Star Wars";
+            this.chkStarWars.UseVisualStyleBackColor = true;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.CadetBlue;
+            this.btnSubmit.Font = new System.Drawing.Font("Stencil", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.Color.White;
+            this.btnSubmit.Location = new System.Drawing.Point(273, 194);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(256, 88);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblInstruction
+            // 
+            this.lblInstruction.AutoSize = true;
+            this.lblInstruction.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstruction.Location = new System.Drawing.Point(0, -1);
+            this.lblInstruction.Name = "lblInstruction";
+            this.lblInstruction.Size = new System.Drawing.Size(624, 55);
+            this.lblInstruction.TabIndex = 3;
+            this.lblInstruction.Text = "Please make your selection:";
+            // 
+            // lblResults
+            // 
+            this.lblResults.AutoSize = true;
+            this.lblResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResults.Location = new System.Drawing.Point(67, 324);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(131, 37);
+            this.lblResults.TabIndex = 4;
+            this.lblResults.Text = "Results:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResults);
+            this.Controls.Add(this.lblInstruction);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.chkStarWars);
+            this.Controls.Add(this.chkStarTrek);
+            this.Name = "Form1";
+            this.Text = "Checkbox Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkStarTrek;
+        private System.Windows.Forms.CheckBox chkStarWars;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblInstruction;
+        private System.Windows.Forms.Label lblResults;
+    }
+}
+
---
+++ Topic 13 - Checkboxes/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Topic_13___Checkboxes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (chkStarTrek.Checked && chkStarWars.Checked)
+            {
+                lblResults.Text = "Results: You are truly a person of culture";
+                btnSubmit.ForeColor = Color.Red;
+                btnSubmit.BackColor = Color.White;
+                this.BackColor = Color.Black;
+                lblResults.ForeColor = Color.White;
+                chkStarTrek.ForeColor = Color.White;
+                chkStarWars.ForeColor = Color.White;
+            }
+            if (chkStarTrek.Checked && !chkStarWars.Checked)
+            {
+                lblResults.Text = "Results: You only like Star Trek!?! Weird...";
+                btnSubmit.ForeColor = Color.White;
+                btnSubmit.BackColor = Color.Black;
+                this.BackColor = Color.Red;
+                lblResults.ForeColor = Color.White;
+                chkStarTrek.ForeColor = Color.White;
+                chkStarWars.ForeColor = Color.White;
+            }
+            if (!chkStarTrek.Checked && chkStarWars.Checked)
+            {
+                lblResults.Text = "Results: You only like Star Wars. Understandable.";
+                btnSubmit.ForeColor = Color.White;
+                btnSubmit.BackColor = Color.Red;
+                this.BackColor = Color.White;
+                lblResults.ForeColor = Color.Black;
+                chkStarTrek.ForeColor = Color.Black;
+                chkStarWars.ForeColor = Color.Black;
+            }
+            if (!chkStarTrek.Checked && !chkStarWars.Checked)
+            {
+                lblResults.Text = "Results: You are wrong.";
+                btnSubmit.ForeColor = Color.Black;
+                btnSubmit.BackColor = Color.Red;
+                this.BackColor = Color.Black;
+                lblResults.ForeColor = Color.White;
+                chkStarTrek.ForeColor = Color.White;
+                chkStarWars.ForeColor = Color.White;
+            }
+        }
+    }
+}
